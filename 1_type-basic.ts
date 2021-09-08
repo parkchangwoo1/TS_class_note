7a7acc74edd08b88f3cdc1d88f5ec9b4f027211c
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-09-08 19:26:42 +0900

    add: 1_type-basic.ts (TS declaration)

---
+++ 1_type-basic.ts
@@ -0,0 +1,26 @@
+// TS 선언
+// string
+let str: string = 'hello';
+// number
+let number: number = 10;
+// Array
+let arr1: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj1: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000
+};
+
+// TS 진위값
+let show: boolean = true;
